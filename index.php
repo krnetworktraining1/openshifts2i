673406b6cb2087dab2eaa2d73c5fb01d9800e897
krnetworktraining1 <30631056+krnetworktraining1@users.noreply.github.com>
2021-06-08 08:23:24 +0530

    Software update

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>hello PHP Test2</title>
  </head>
  <body>
- <?php echo '<p>Developer source code update</p>'; ?> 
+ <?php echo '<p>software  has been updated now</p>'; ?> 
  </body>
 </html>
