5aa019808fbe8cb7ee73785a246934c32aed6b29
deep222-tcs <61450650+deep222-tcs@users.noreply.github.com>
2021-06-18 10:21:20 +0530

    updated

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>hello PHP Test2</title>
  </head>
  <body>
- <?php echo '<p>again change source code</p>'; ?> 
+ <?php echo '<p>again change source code on Friday</p>'; ?> 
  </body>
 </html>
